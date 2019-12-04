39330d2912b54788d1936a6b3eb73dac8439c609
Rikki Schulte <richard.j.schulte@gmail.com>
2019-12-04 18:50:37 -0500

    chore: improve documentation and remove errant build output

--- packages/graphiql/resources/webpack.config.js
+++ packages/graphiql/resources/webpack.config.js
@@ -17,9 +17,9 @@ const resultConfig = {
         'react-hot-loader/patch', // activate HMR for React
         'webpack-dev-server/client?http://localhost:8080', // bundle the client for webpack-dev-server and connect to the provided endpoint
         'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-        './index.js', // the entry point of our app
+        './cdn.js', // the entry point of our app
       ]
-    : './index.js',
+    : './cdn.js',
   context: rootPath('src'),
   output: {
     path: rootPath(),
@@ -117,7 +117,7 @@ if (process.env.ANALYZE) {
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
       openAnalyzer: false,
-      reportFilename: rootPath('coverage/analyzer/index.html'),
+      reportFilename: rootPath('analyzer.html'),
     })
   );
 }
